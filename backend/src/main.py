374d37cc5584e26933b51d25511d711f55218f5f
TheMrKan <mrkan2017@gmail.com>
2025-10-24 16:56:08 +0300

    отправка

--- backend/src/main.py
+++ backend/src/main.py
@@ -3,11 +3,23 @@ import src.logs
 src.logs.setup()
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.api.issue import router as issue_router
 from src.core.container import Container
 
 app = FastAPI()
+
+origins = [
+    "*",  # Allows all origins (not recommended for production)
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"], # allow methods
+    allow_headers=["*"], # allow headers
+)
 
 
 Container.build()

e7679ce99746845037e16735fc05c80c8f486fa7
plll666 <kolalazzzz@gmail.com>
2025-11-20 22:36:23 +0300

    добавил апи пользователя

--- backend/src/api/schemas.py
+++ backend/src/api/schemas.py
@@ -38,3 +38,15 @@ class Token(BaseModel):
 class TokenData(BaseModel):
     user_id: Optional[str] = None
     email: Optional[str] = None
+
+
+class ProfileResponse(BaseModel):
+    id: uuid.UUID
+    email: str
+    first_name: Optional[str]
+    last_name: Optional[str]
+    avatar_url: Optional[str]
+    created_at: datetime
+
+    class Config:
+        from_attributes = True

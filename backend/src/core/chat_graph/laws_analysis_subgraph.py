a82f3b50dc01fe3fb6eae2e834525e23f4b7c085
TheMrKan <mrkan2017@gmail.com>
2025-11-20 17:27:14 +0300

    структура первого подграфа для цикла вопрос-ответов. Промпт запуска цикла

--- backend/src/core/chat_graph/laws_analysis_subgraph.py
+++ backend/src/core/chat_graph/laws_analysis_subgraph.py
@@ -1,4 +1,5 @@
 from langgraph.graph import StateGraph, START, END
+from langgraph.types import interrupt
 import logging
 
 from src.core.chat_graph.common import BaseState, InputState, create_process_confirmation_node
@@ -21,12 +22,22 @@ class LawsAnalysisSubgraph(StateGraph[BaseState, None, InputState, BaseState]):
     def __build(self):
         self.add_edge(START, "save_first_info")
         self.add_node("save_first_info", self.__save_first_info)
-        self.add_edge("save_first_info", "find_law_documents")
+        self.add_edge("save_first_info", "analyze_info")
+
+        self.add_node("analyze_info", self.__analyze_info)
+        self.add_conditional_edges("analyze_info", self.__select_path, {
+            "handle_answer": "handle_answer",
+            "find": "find_law_documents",
+            "END": END
+        })
+
+        self.add_node("handle_answer", self.__handle_answer)
+        self.add_edge("handle_answer", "analyze_info")
 
         self.add_node("find_law_documents", self.__find_law_documents)
-        self.add_edge("find_law_documents", "analyze_first_info")
+        self.add_edge("find_law_documents", "analyze_law_documents")
 
-        self.add_node("analyze_first_info", self.__analyze_first_info)
+        self.add_node("analyze_first_info", self.__analyze_law_documents)
         self.add_conditional_edges("analyze_first_info", self.__continue_if_true("can_help"), {
             "continue": "confirm0",
             "END": END
@@ -49,7 +60,7 @@ class LawsAnalysisSubgraph(StateGraph[BaseState, None, InputState, BaseState]):
         return {"law_docs": docs}
 
     @inject_global
-    async def __analyze_first_info(self, state: BaseState, llm: LLMABC) -> BaseState:
+    async def __analyze_law_documents(self, state: BaseState, llm: LLMABC) -> BaseState:
         acts_analysis_result = await llm_use_cases.analyze_acts_async(llm, state["messages"], state["law_docs"])
         self.__logger.info(f"Acts analysis result: {acts_analysis_result}")
         return {"can_help": acts_analysis_result.can_help, "messages": acts_analysis_result.messages}
